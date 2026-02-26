c6f0df00505268d882bfc6df37b9b24d11182915
alirezass <alirezakhosraviss1526@gmail.com>
2026-02-26 09:35:15 +0330

    prop drilling and note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,33 @@
+
+function Note({deleteNote, note}) {
+  return (
+        <div
+          className="p-4 bg-white rounded-lg shadow-md border-l-4"
+          style={{
+          borderLeftColor: 
+          note.priority === 'High'
+          ? 'red'
+          : note.priority === 'Medium'
+          ? 'orange'
+          : 'green'
+          }}
+        >
+          <h3 className="text-lg font-bold">{note.title}</h3>
+          <p className="text-sm text-gray-600">
+            <strong>Category:</strong> {note.category}
+          </p>
+          <p className="text-sm mt-3 text-gray-600">
+            <strong>Priority:</strong> {note.priority}
+          </p>
+          <p className="mt-2">{note.description}</p>
+          <button
+            className="mt-3 text-red-500 hover:text-red-700 cursor-pointer transition"
+            onClick={() => deleteNote(note.id)}
+          >
+            🗑 Delete
+          </button>
+        </div>
+  )
+}
+
+export default Note
